3bbcc82bcf2cc5cf66fe4d7b7ba33e43abffa4c7
R0f1ida <kermiche.rofaida23@gmail.com>
2025-03-10 21:43:05 +0100

    responsive homepage

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -7,7 +7,7 @@ function Layout() {
         <nav className="navbar navQ navbar-expand-lg navbar-light bg-white">
             <div className="container-fluid">
                 <Link className="navbar-brand" to="/">
-                    <img src={logo} alt="logo" width="50" />
+                    <img src={logo} alt="logo"/>
                 </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
