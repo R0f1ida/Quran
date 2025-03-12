24ae478b8cf1b40ba70eb4b48af558d7c4a7aab1
R0f1ida <kermiche.rofaida23@gmail.com>
2025-03-12 21:45:30 +0100

    rebuild

--- src/pages/Quran.js
+++ src/pages/Quran.js
@@ -1,8 +1,20 @@
 import { useEffect, useState } from "react";
-import { fetchSurrah } from "../components/helper";
+import { fetchAyah, fetchSurrah } from "../components/helper";
 
 export function Quran() {
     const [sowar, setSowar] = useState([]);
+    // const [surrah, setSurrah] = useState([]);
+    const getSurrah = async (e) => {
+        // setSurrah(fetchedSurrah);
+        if(e.target.classList.contains('sowar--item')) {
+            let surrahNumber = e.target.querySelector('.sowar--number');
+            const fetchedSurrah = await fetchAyah(surrahNumber);
+            console.log(fetchedSurrah);
+
+        }else{
+            console.log(e.target);
+        }
+    }
     useEffect(() => {
         const fetchData = async () => {
         const fetchedSowar = await fetchSurrah();
@@ -13,9 +25,12 @@ export function Quran() {
     }, []);
     return (
         <div>
+            <div>
+                <h1>Quran</h1>
+            </div>
             <ul className='sowar'>
                 {sowar.map(s=> 
-                    <li className="sowar--item">
+                    <li className="sowar--item" onClick={getSurrah}>
                         <span className="sowar--number">{s.number}</span>
                         <div className="sowar--name">{s.englishName}</div>
                         <div className="sowar--arab">{s.name}</div>    
