0943a6a2b23ea2a572e7f16029b8440ec6cc2d03
R0f1ida <kermiche.rofaida23@gmail.com>
2025-03-15 21:07:23 +0100

    add index link

--- src/pages/Quran.js
+++ src/pages/Quran.js
@@ -6,13 +6,13 @@ export function Quran() {
     const [surrah, setSurrah] = useState([]);
     const getSurrah = async (e) => {
         // setSurrah(fetchedSurrah);
-        if(e.target.classList.contains('sowar--item')) {
-            const surrahNumber = e.target.querySelector('.sowar--number');
+        if(e.target.classList.contains('quran__sowar--item')) {
+            const surrahNumber = e.target.querySelector('.quran__sowar--number');
             const fetchedSurrah = await fetchAyah(surrahNumber.textContent);
             setSurrah(fetchedSurrah);
             console.log(surrah);
         }else {
-            const surrahNumber = e.target.parentElement.querySelector('.sowar--number');
+            const surrahNumber = e.target.parentElement.querySelector('.quran__sowar--number');
             const fetchedSurrah = await fetchAyah(surrahNumber.textContent);
             setSurrah(fetchedSurrah);
             console.log(surrah);
@@ -28,16 +28,25 @@ export function Quran() {
         fetchData();
     }, [sowar]);
     return (
-        <div>
-            <div>
-                <div>{surrah.map(ayah => `${ayah.text} ${ayah.numberInSurah}`).join('')}</div>
+        <div className="quran">
+            <a href="#sowar" className="quran__link">go to index &darr;</a>
+            
+            <div className="quran__surrah">
+                {/* <button className="quran__surrah--prev">prev</button>
+                <button className="quran__surrah--next">next</button>
+                 */}
+                <button className="quran__surrah--audio">play</button>
+                <audio src="https:\/\/cdn.islamic.network\/quran\/audio\/128\/ar.alafasy\/2.mp3" controls></audio>
+                <div>
+                    {surrah.map(ayah => `${ayah.text} ${ayah.numberInSurah}`).join(' ')}
+                </div>    
             </div>
-            <ul className='sowar'>
+            <ul className='quran__sowar' id="sowar">
                 {sowar.map(s=> 
-                    <li className="sowar--item" onClick={getSurrah}>
-                        <span className="sowar--number">{s.number}</span>
-                        <div className="sowar--name">{s.englishName}</div>
-                        <div className="sowar--arab">{s.name}</div>    
+                    <li className="quran__sowar--item" onClick={getSurrah}>
+                        <span className="quran__sowar--number">{s.number}</span>
+                        <div className="quran__sowar--name">{s.englishName}</div>
+                        <div className="quran__sowar--arab">{s.name}</div>    
                     </li> )}
             </ul>
         </div>
