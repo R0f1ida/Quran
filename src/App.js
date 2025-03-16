0c24f3310c9c9926c4648610b76c6a5f7131b88c
R0f1ida <kermiche.rofaida23@gmail.com>
2025-03-16 11:31:52 +0100

    add top button

--- src/App.js
+++ src/App.js
@@ -3,9 +3,25 @@ import { Footer } from './components/Footer';
 import { Navbar } from './components/Navbar';
 import { BrowserRouter } from 'react-router-dom';
 
+let mybutton = document.getElementById("topBtn");
+
 function App() {
+  window.onscroll = function() {scrollFunction()};
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
   return (
     <div className="App">
+      <button onClick={topFunction} id="topBtn" title="Go to top">Top</button>
       <BrowserRouter>
         <Navbar />
       </BrowserRouter>
